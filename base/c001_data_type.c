0c00df967d4e2d78bdbfd63b20bdcdba039b0ec7
MAAAAAAAN <togeohot@tom.com>
2021-04-07 12:51:50 +0800

    0407211251

---
+++ base/c001_data_type.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	printf("char->%d\n", sizeof(char));
+	printf("short->%d\n", sizeof(short));
+	printf("int->%d\n", sizeof(int));
+	printf("long->%d\n", sizeof(long));
+	printf("float->%d\n", sizeof(float));
+	printf("double->%d\n", sizeof(double));
+	
+	return 0;
+} 
---
+++ base/c002_typedef.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+typedef short int INT16;
+int main()
+{
+	INT16 a = 101;
+	short int b = 101;
+	printf("%d\n", a);
+	printf("%d\n", b);
+	
+	return 0;
+}
